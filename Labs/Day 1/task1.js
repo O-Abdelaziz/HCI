595c9d4152325da7c6a06934bac4c83d7ad610c6
Fatma-Elsayed-Hussien <fatmaelsayedhussien@gmail.com>
2024-02-21 19:46:44 +0200

    task 1

---
+++ Labs/Day 1/task1.js
@@ -0,0 +1,18 @@
+// ***************** Task 1 **************
+// using prompt
+//    - take no of persons from user 
+//    - for example, if you enter 3:
+//          - take names of 3 persons (prompt for name show 3 times)
+//          - take ages of 3 persons  (prompt for age show 3 times)
+//    - Add/Push names to ArrayNames
+//    - Add/Push ages to ArrayAges
+//    - Then show all names & ages in console
+
+// ✨✨ NOTES ✨✨
+//      - name should be string
+//      - name length should be more than 2 and less than 12
+//      - age should be number
+//      - age should be more than 18 and less than 60
+
+// 👉 If the conditions are not met, ignore the value entered by the user 
+//  and show the prompt again
